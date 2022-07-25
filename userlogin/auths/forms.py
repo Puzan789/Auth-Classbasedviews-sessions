6be718b2562022931e5a4b8db64c3a3eeb0173cd
puzan789 <puzan936@gmail.com>
2022-07-25 23:26:28 +0545

    updates

--- userlogin/auths/forms.py
+++ userlogin/auths/forms.py
@@ -14,5 +14,11 @@ class edituserprofile(UserChangeForm):
         model=User
         fields=['username','first_name','last_name','email','date_joined','last_login','is_active']
         labels={'email':'EMAIL'}
+class editadminprofile(UserChangeForm):
+    password=None
+    class Meta:
+        model=User
+        fields='__all__'
+        labels={'email':'EMAIL'}
 
        
--- userlogin/auths/views.py
+++ userlogin/auths/views.py
@@ -1,24 +1,25 @@
 
 from django.contrib import messages
 from django.shortcuts import render,HttpResponseRedirect
-from django.urls import is_valid_path
-from .forms import signupform,edituserprofile
+from .forms import signupform,edituserprofile,editadminprofile
 from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm,SetPasswordForm,UserChangeForm
 from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+from django.contrib.auth.models import User
+#user signup views
 def signup(request):
     if request.method == "POST":
         fm=signupform(request.POST)
         if fm.is_valid():
             messages.success(request,'!!!!your account is created!!!!')
             fm.save()
-        fm=signupform()
-        print('done')
+            fm=signupform()
+            print('done')
     else:
         fm=signupform()
         print("error")
 
     return render(request,'auths/signup.html',{'form':fm})
-
+#userlogin views
 def userlogin(request):
     if not request.user.is_authenticated:
         if request.method=="POST":
@@ -36,28 +37,41 @@ def userlogin(request):
         return render(request,'auths/login.html',{'form':fm})
     else:
         return HttpResponseRedirect('/profile/')
+#userprofile views
 def userprofile(request):
     if request.user.is_authenticated:
         if request.method=="POST":
-            fm=edituserprofile(request.POST,instance=request.user)
+            if request.user.is_superuser == True:
+                fm=editadminprofile(request.POST,instance=request.user)
+                users=User.objects.all()
+            else:
+                users=None
+                fm=edituserprofile(request.POST,instance=request.user)
             if fm.is_valid():
                 fm.save()
                 print("saved")
         else:
-            fm=edituserprofile(instance=request.user)
-        return render(request, 'auths/profile.html' ,{'name':request.user,'form':fm})
+            if request.user.is_superuser==True:
+                users=User.objects.all()
+                fm=editadminprofile(instance=request.user)
+            else: 
+                users=None
+                fm=edituserprofile(instance=request.user)
+        return render(request, 'auths/profile.html' ,{'name':request.user,'form':fm,'users':users})
     else:
         return HttpResponseRedirect('/login/')
+#user logout views
 def userlogout(request):
     logout(request)
     return HttpResponseRedirect('/login/')
+# change password wih using old password
 def changepass(request):
     if request.user.is_authenticated:
         if request.method=="POST":
             fm=PasswordChangeForm(user=request.user,data=request.POST)
             if fm.is_valid():
                 fm.save()
-                update_session_auth_hash(request,fm.user)
+                update_session_auth_hash(request,fm.user) #will redirect to your profile page
                 print("saved")
                 return HttpResponseRedirect('/profile/')
         else:
@@ -65,7 +79,7 @@ def changepass(request):
         return render(request,'auths/changepass.html',{'form':fm})
     else:
         return HttpResponseRedirect('/login/')
-
+# change password without using old password
 def changepass1(request):
     if request.user.is_authenticated:
         if request.method=="POST":
